fa1ea321474f5bdb89925b892a0b994c8330bdd0
shiva <qianwei4712@163.com>
2019-12-29 20:52:22 +0800

    v-for添加key，消除警告； 添加三个链接

--- src/components/chest/resourceNavigation/ResourceNavigation.vue
+++ src/components/chest/resourceNavigation/ResourceNavigation.vue
@@ -7,14 +7,14 @@
                     <h2>{{eachType.name}}</h2>
                     <div>
                         <el-row :gutter="36">
-                            <el-col :span="4" v-for="item in eachType.data">
-                                <a :href="item.href" target="_blank">
+                            <el-col :span="4" v-for="(outerChain, index) in eachType.data" :key="index">
+                                <a :href="outerChain.href" target="_blank">
                                     <el-card shadow="hover">
                                         <div class="item-title">
-                                            <img :src="item.img" class="item-title-img">
-                                            <div class="item-title-name">{{item.title}}</div>
+                                            <img :src="outerChain.img" class="item-title-img" alt="" />
+                                            <div class="item-title-name">{{outerChain.title}}</div>
                                         </div>
-                                        <div :title="item.desc" class="item-desc">{{item.desc}}</div>
+                                        <div :title="outerChain.desc" class="item-desc">{{outerChain.desc}}</div>
                                     </el-card>
                                 </a>
                             </el-col>
@@ -46,10 +46,11 @@
                         ]
                     },
                     {
-                        name:'源码阅读',
+                        name:'源码练习',
                         data:[
                             {title:'Gitee', href:'https://gitee.com/', img:require('@/assets/chest/img/gitee.png'), desc:'开源中国旗下的代码托管平台：码云' },
-                            {title:'GitHub', href:'https://github.com/', img:require('@/assets/chest/img/github.png'), desc:'GitHub是一个面向开源及私有软件项目的托管平台' }
+                            {title:'GitHub', href:'https://github.com/', img:require('@/assets/chest/img/github.png'), desc:'GitHub是一个面向开源及私有软件项目的托管平台' },
+                            {title:'LeetCode', href:'https://leetcode-cn.com/problemset/all/', img:require('@/assets/chest/img/leetcode.svg'), desc:'leetcode中国区，算法在线训练' }
                         ]
                     },
                     {
@@ -57,7 +58,7 @@
                         data:[
                             {title:'', href:'https://www.runoob.com/', img:require('@/assets/chest/img/cainiao.png'), desc:'菜鸟教程集合' },
                             {title:'W3School', href:'https://www.w3school.com.cn/', img:require('@/assets/chest/img/w3school.png'), desc:'W3School 是因特网上最大的 WEB 开发者资源，其中包括全面的教程、完善的参考手册以及庞大的代码库' },
-                            {title:'W3Cschool', href:'https://www.w3cschool.cn/tutorial', img:require('@/assets/chest/img/w3cschool.png'), desc:'w3cschool主要为初学者技术的人员提供在线学习教程和日常技术资料查询服务' },
+                            {title:'W3Cschool', href:'https://www.w3cschool.cn/tutorial', img:require('@/assets/chest/img/w3cschool.png'), desc:'w3cschool主要为初学者技术的人员提供在线学习教程和日常技术资料查询服务' }
                         ]
                     },
                     {
@@ -74,16 +75,18 @@
                             {title:'Layui', href:'https://www.layui.com/doc/', img:require('@/assets/chest/img/layui.png'), desc:'一款采用自身模块规范编写的前端 UI 框架' },
                             {title:'VUE', href:'https://cn.vuejs.org/', img:require('@/assets/chest/img/vuejs.png'), desc:'渐进式JavaScript 框架' },
                             {title:'Element-UI', href:'https://element.eleme.cn/#/zh-CN', img:require('@/assets/chest/img/element.png'), desc:'Element，一套为开发者、设计师和产品经理准备的基于 Vue 2.0 的桌面端组件库' },
-                            {title:'JQuery API', href:'http://jquery.cuishifeng.cn/', img:require('@/assets/chest/img/jquery.png'), desc:'一个快速、简洁的JavaScript框架' }
+                            {title:'JQuery API', href:'http://jquery.cuishifeng.cn/', img:require('@/assets/chest/img/jquery.png'), desc:'一个快速、简洁的JavaScript框架' },
+                            {title:'ECharts', href:'https://www.echartsjs.com/examples/zh/index.html', img:require('@/assets/chest/img/echarts.png'), desc:'百度开发的可定制的数据可视化图表, 已经捐给Apache' }
                         ]
                     },
                     {
                         name:'其他资源',
                         data:[
-                            {title:'ECharts', href:'https://www.echartsjs.com/examples/zh/index.html', img:require('@/assets/chest/img/echarts.png'), desc:'百度开发的可定制的数据可视化图表, 已经捐给Apache' },
                             {title:'脚本之家', href:'http://tools.jb51.net/', img:require('@/assets/chest/img/jiaobenzhijia.png'), desc:'脚本之家旗下的工具箱' },
                             {title:'iconfont', href:'https://www.iconfont.cn/', img:require('@/assets/chest/img/iconfont.png'), desc:'阿里妈妈MUX倾力打造的矢量图标管理、交流平台。设计师将图标上传到Iconfont平台，用户可以自定义下载多种格式的icon，平台也可将图标转换为字体，便于前端工程师自由调整与调用' },
-                            {title:'智图', href:'https://zhitu.isux.us/', img:require('@/assets/chest/img/zhitu.png'), desc:'腾讯出品 在线图片压缩 支持转成 webP 处理静态图片时候很好用' }
+                            {title:'智图', href:'https://zhitu.isux.us/', img:require('@/assets/chest/img/zhitu.png'), desc:'腾讯出品在线图片压缩 支持转成 webP 处理静态图片时候很好用' },
+                            {title:'processOn', href:'https://www.processon.com/', img:require('@/assets/chest/img/processOn.png'), desc:'免费在线作图、实时协作，支持流程图、思维导图、原型图、UML、网络拓扑图、组织结构图等' },
+                            {title:'On Java 8', href:'https://lingcoder.gitee.io/onjava8/', img:require('@/assets/chest/img/onjava8.jpg'), desc:'Java编程思想第五版，大神翻译中文版' }
                         ]
                     }
                 ]
