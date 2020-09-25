268b9dfe7b442c8f9871b6833ed1f54e19739a2a
路的尽头在哪 <qianwei4712@163.com>
2020-09-25 20:30:35 +0800

    公安备案

--- src/components/incloude/Footer.vue
+++ src/components/incloude/Footer.vue
@@ -3,8 +3,9 @@
         <el-footer>
             <div class="footer-div">
                 <div class="footer-record-left read-only">
-                    Made with <span class="footer-span-heart">♥</span> for the people of the internet -
-                    <a href="http://beian.miit.gov.cn/" target="_blank" style="color: rgb(158, 158, 158)">浙ICP备18006899号-2 &nbsp;&nbsp; ©shiva</a>
+                    Made with <span class="footer-span-heart">♥</span> for the people of the internet - ©shiva |
+                    <a href="http://beian.miit.gov.cn/" target="_blank" style="color: rgb(158, 158, 158)">浙ICP备18006899号-2 </a> &nbsp;|&nbsp;
+                    <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33028302000356" style="color: rgb(158, 158, 158);display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img :src="gaba" style="float:left;"/>浙公网安备 33028302000356号</a>
                 </div>
                 <div class="footer-func-right">
                     <a href="mailto:qianwei4712@163.com" title="Email联系作者">
@@ -28,7 +29,8 @@
         name: "Footer",
         data(){
             return{
-                src:require('@/assets/home/img/thanks.png')
+                src:require('@/assets/home/img/thanks.png'),
+                gaba:require('@/assets/home/img/gaba.png')
             }
         }
     }
