a0ef086f0bcc0ceceabfc098ed1aaed1e4052a4c
路的尽头在哪 <qianwei4712@163.com>
2021-09-23 23:42:52 +0800

    参考 typora 的 Mo 主题，修改样式

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import 'github-markdown-css/github-markdown.css';
-import 'highlight.js/styles/googlecode.css';
+import 'highlight.js/styles/idea.css';
 import './assets/blog/blog.css';
 import './assets/chest/chest.css';
 import BlogCommon from './assets/blog/blog.js';
